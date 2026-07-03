f708557e0f61e10aef80fa36d56fd11795986f2f
khoaddb2207532 <133483029+khoaddb2207532@users.noreply.github.com>
2026-07-03 14:45:35 +0700

    update parasm

--- pipelines/stage5_train_rule_regularized.py
+++ pipelines/stage5_train_rule_regularized.py
@@ -64,6 +64,8 @@ def main(params_path: str) -> None:
         "freeze_schedule": freeze_schedule,
         "monitor_metric": params.get("monitor_metric", "val_acc"),
         "use_scheduler": True,
+        "scheduler_factor": 0.1,
+        "scheduler_patience": 3,
         "dvclive_path": os.path.join(save_dir, "dvclive_rule_regularized"),
         "save_dir": save_dir,
     }
